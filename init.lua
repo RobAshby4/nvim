af05f8ae59ef20102ad2f84006af09595c4087e2
RobAshby4 <robert.n.ashby@gmail.com>
2025-07-20 17:40:01 -0700

    added files

---
+++ init.lua
@@ -0,0 +1,16 @@
+-- usr/plugin configs
+require("config.lazy")
+require("config.keybinds")
+
+-- lsp configs
+require("config.lsp_config")
+require("config.lsp.clangd")
+require("config.lsp.luals")
+
+-- nvim settings 
+vim.opt.number = true
+vim.opt.wrap = false
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.cmd [[colorscheme gruvsquirrel]]
